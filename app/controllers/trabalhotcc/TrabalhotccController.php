ed8494a411eca7240011d7610edf728b75bc9054
Marcu5s <marcusjavascript@gmail.com>
2015-06-25 10:11:00 -0300

    correcao cadastro

--- app/controllers/trabalhotcc/TrabalhotccController.php
+++ app/controllers/trabalhotcc/TrabalhotccController.php
@@ -30,18 +30,23 @@ class TrabalhotccController extends \core\app\Controller {
             
             
            $file = UploadFile::load($model, 'file');
-                      
+           
+           if(!empty($file)){           
+
            $widimage = WideImage::load($file->tmpName);
-            
+           
+           $model->file  = $file->name; 
+
            $resize = $widimage->resize(255,255);
                       
            $filename = WWW_ROOT.'/app/assets/arquivos/profile/'.$file->name;
            $resize->saveToFile($filename);
            
            chmod($filename,0777);
-             
-           $model->senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
-           $model->file  = $file->name; 
+           }  
+
+           $model->senha = password_hash($model->senha, PASSWORD_DEFAULT);
+           
                       
            if($model->save())
               $this->Json([
